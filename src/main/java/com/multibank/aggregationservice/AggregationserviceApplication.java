ac3ab0498ad953cb3eea0f26717c9b9cf8cf592f
Mahesh Saini <mahesh.saini@dealshare.in>
2026-02-28 13:37:10 +0400

    Initial commit - repo setup

---
+++ src/main/java/com/multibank/aggregationservice/AggregationserviceApplication.java
@@ -0,0 +1,13 @@
+package com.multibank.aggregationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregationserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multibank/aggregationservice/AggregationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multibank.aggregationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
