6ca4a64ad8d45ce32dbc22fe56fbaa71d51bd941
Mahesh Saini <mahesh.saini@dealshare.in>
2026-02-28 14:37:59 +0400

    test commit

--- src/main/java/com/multibank/aggregationservice/AggregationserviceApplication.java
+++ src/main/java/com/multibank/aggregationservice/AggregationserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AggregationserviceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("App started");
 		SpringApplication.run(AggregationserviceApplication.class, args);
 	}
 
